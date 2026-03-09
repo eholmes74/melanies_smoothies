0b89e77631865828f1082005e68917526bd8401f
Eric Holmes <eric.holmes@cellularsales.com>
2026-03-09 13:11:26 -0400

    Update streamlit_app.py
    ADDED NUTTRITION INFO CODE

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,8 @@ if (ingredients_list and name_on_order):
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!'+' '+name_on_order, icon="✅")
+
+### NEW SECTION TO DISPLAY SMOOTHIE FRUIT NUTRITION INFORMATION
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
