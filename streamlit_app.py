a7f353f3e64d859bbb64ee2d45a351a6684bdfeb
Eric Holmes <eric.holmes@cellularsales.com>
2026-03-09 13:38:40 -0400

    Refactor smoothie order logic and API call placement
    Added requests import and moved API call inside the loop for each selected fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 
@@ -32,6 +33,8 @@ if (ingredients_list and name_on_order):
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -48,11 +51,6 @@ if (ingredients_list and name_on_order):
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!'+' '+name_on_order, icon="✅")
-
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
